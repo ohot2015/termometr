311a07915bbd6351c08b72b59eb74d853a493bdb
banel1ng <ohot2015@gmail.com>
2020-12-22 18:52:07 +0300

    fix mb_subbstr

--- input.php
+++ input.php
@@ -2,8 +2,8 @@
 ini_set('display_errors', 1);
 $data = file_get_contents('php://input');
 $file = file_get_contents('./log.json');
-$data = mb_substr($file, 0, -1);
-$file = mb_substr($file, 0, 1);
+$data = substr($file, 0, -1);
+$file = substr($file, 0, 1);
 $data .= $file . ']';
 file_put_contents('./log.json', $data);
 ?>
