1ffb89895468eb974c66fa238793456a2b65d153
banel1ng <ohot2015@gmail.com>
2020-12-22 19:09:00 +0300

    fix subbstr logs

--- input.php
+++ input.php
@@ -2,8 +2,10 @@
 ini_set('display_errors', 1);
 $data = file_get_contents('php://input');
 $file = file_get_contents('./log.json');
-$data = substr($file, 0, -1);
-$file = substr($file, 0, 1);
-$data .= $file . ']';
+if (!empty($file)) {
+    $data = substr($data, 0, -1);
+    $file = substr($file, 1);
+    $data .= ','.$file;
+}
 file_put_contents('./log.json', $data);
 ?>
