a61bc93278026b7ff8a5bc1b26372645ec03d18c
banel1ng <ohot2015@gmail.com>
2020-12-22 18:09:10 +0300

    fix define and comment

--- termometr.ino
+++ termometr.ino
@@ -1,19 +1,23 @@
+#define tmax 96 // температура максимум
+#define tmin 86 // температура минимум
+#define timeout 10 // период снятия показаний в секундах
+
 #define B 3600 // B-коэффициент
 #define SERIAL_R 2940 // сопротивление последовательного резистора, 102 кОм
 #define THERMISTOR_R 10860 // номинальное сопротивления термистора, 100 кОм
 #define NOMINAL_T 21 // номинальная температура (при которой TR = 100 кОм)
  
-// const byte tempPin = A5;
+const byte tempPin = A5;
 const byte relePin = 2;
 bool releMod = false;
 void setup() {
     Serial.begin( 9600 );
-    pinMode( A5, INPUT );
+    pinMode( tempPin, INPUT );
     pinMode( relePin, OUTPUT);
 }
  
 void loop() {
-    int t = analogRead( A5 );
+    int t = analogRead( tempPin );
     float tr = 1023.0 / t - 1;
     tr = SERIAL_R / tr;
     Serial.print("R=");
@@ -21,22 +25,22 @@ void loop() {
     Serial.print(", t=");
  
     float steinhart;
-    steinhart = tr / THERMISTOR_R; // (R/Ro)
-    steinhart = log(steinhart); // ln(R/Ro)
-    steinhart /= B; // 1/B * ln(R/Ro)
-    steinhart += 1.0 / (NOMINAL_T + 273.15); // + (1/To)
-    steinhart = 1.0 / steinhart; // Invert
+    steinhart = tr / THERMISTOR_R;
+    steinhart = log(steinhart);
+    steinhart /= B;
+    steinhart += 1.0 / (NOMINAL_T + 273.15);
+    steinhart = 1.0 / steinhart;
     steinhart -= 273.15; 
     
-    if (steinhart >95) { // 94
+    if (steinhart >tmax) {
       releMod = true;
     }
-    if (steinhart < 90) { //88
+    if (steinhart < tmin) {
       releMod = false;
     }
     digitalWrite(relePin, releMod);  
     Serial.print(steinhart);  
-   Serial.print(", pinMod=");
-   Serial.println(releMod);
-    delay(100);
+    Serial.print(", pinMod=");
+    Serial.println(releMod);
+    delay(timeout * 1000);
 }
