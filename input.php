3671c0e84bb26f0987f0d6f751d59b77e0de7b38
banel1ng <ohot2015@gmail.com>
2021-01-07 19:12:57 +0300

    fix data subustr

--- input.php
+++ input.php
@@ -3,10 +3,11 @@ ini_set('display_errors', 1);
 $data = file_get_contents('php://input');
 $file = file_get_contents('./log.json');
 if (!empty($file)) {
-    $data = substr($data, 0, -1);
-    $file = substr($file, 1);
-    $data .= ','.$file;
+    $file = substr($file, 0, 1);
+    $data = substr($data, 1);
+    $file .=  ',' . $data;
+    //$data .= ','.$file;
 
 }
-file_put_contents('./log.json', $data);
+file_put_contents('./log.json', $file);
 ?>
