fa1f0522e0736d4c11f1a3527d4a8b4243fc259f
banel1ng <ohot2015@gmail.com>
2021-07-27 17:40:11 +0300

    add tg hook

---
+++ hook.php
@@ -0,0 +1,17 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+$bot_api_key  = '1845276413:AAG0BJt1zIxMy18OvvH_c31NOB2asnCQ7XY';
+$bot_username = 'pathePolivBot';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Handle telegram webhook request
+    $telegram->handle();
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Silence is golden!
+    // log telegram errors
+    // echo $e->getMessage();
+}
---
+++ tgSetWebHook.php
@@ -0,0 +1,21 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$bot_api_key  = '1845276413:AAG0BJt1zIxMy18OvvH_c31NOB2asnCQ7XY';
+$bot_username = 'pathePolivBot';
+$hook_url     = 'https://crm.m2lab.ru:3100/hook.php';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // log telegram errors
+    // echo $e->getMessage();
+}
