aa3003fb0e8faa8d2dc955d4c482dc5c6f4cba94
banel1ng <ohot2015@gmail.com>
2021-07-27 18:34:50 +0300

    add vardumper

--- hook.php
+++ hook.php
@@ -1,4 +1,5 @@
 <?php
+ ;
 // Load composer
 require __DIR__ . '/vendor/autoload.php';
 $bot_api_key  = '1845276413:AAG0BJt1zIxMy18OvvH_c31NOB2asnCQ7XY';
@@ -7,11 +8,15 @@ $bot_username = 'pathePolivBot';
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
-    //dump($telegram);exit;
-    // Handle telegram webhook request
+
+
+    $str = 'Начал поливать';
+    $result = \Longman\TelegramBot\Request::sendMessage([
+        'chat_id' => "-457457807",
+        'text'    => $str,
+    ]);
+    dump($result);
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    // Silence is golden!
-    // log telegram errors
-    // echo $e->getMessage();
+    echo $e->getMessage();
 }
