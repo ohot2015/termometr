fdce62a9b74830dbad564ae79305c638a15d045a
banel1ng <ohot2015@gmail.com>
2021-07-31 20:46:13 +0300

    fix

--- tgTermoatatHook.php
+++ tgTermoatatHook.php
@@ -7,7 +7,7 @@ $banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
 try {
     $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
     $rs = file_get_contents('php://input');
-    $jsonRs = '';//json_decode($rs,true);
+    $jsonRs = json_decode($rs,true);
     file_put_contents('./test.log',$rs);
     $result = \Longman\TelegramBot\Request::sendMessage([
         'chat_id' => "-411683583",
