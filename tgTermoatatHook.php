5d1f9b13d3677b423cf4a1e7d361497695d1a5cc
banel1ng <ohot2015@gmail.com>
2021-07-31 20:57:50 +0300

    fix

--- tgTermoatatHook.php
+++ tgTermoatatHook.php
@@ -8,11 +8,35 @@ try {
     $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
     $rs = file_get_contents('php://input');
     $jsonRs = json_decode($rs,true);
-    file_put_contents('./test.log',$rs);
-    $result = \Longman\TelegramBot\Request::sendMessage([
-        'chat_id' => "-411683583",
-        'text'    =>$jsonRs['message']['text'] ,
-    ]);
+
+    if ($jsonRs['message']['text'] == "debug") {
+        file_put_contents('./commandTermostat.log', 'debug');
+        $result = \Longman\TelegramBot\Request::sendMessage([
+            'chat_id' => "-411683583",
+            'text'    => 'установлен дебаг мод' ,
+        ]);
+    }
+
+    if (false != strstr($jsonRs['message']['text'] , 'tmax-')
+        && count($jsonRs['message']['text']) >= 5
+        && count($jsonRs['message']['text']) <= 7) {
+        file_put_contents('./commandTermostat.log', $jsonRs['message']['text'] );
+        $result = \Longman\TelegramBot\Request::sendMessage([
+            'chat_id' => "-411683583",
+            'text'    => 'максимальная температура отправлена на термостат' ,
+        ]);
+    }
+
+    if (false != strstr($jsonRs['message']['text'] , 'tmin-')
+        && count($jsonRs['message']['text']) >= 5
+        && count($jsonRs['message']['text']) <= 7) {
+        file_put_contents('./commandTermostat.log', $jsonRs['message']['text'] );
+        $result = \Longman\TelegramBot\Request::sendMessage([
+            'chat_id' => "-411683583",
+            'text'    => 'минимальная температура отправлена на термостат' ,
+        ]);
+    }
+
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     echo $e->getMessage();
