ab3ec513fda608121ca089ed1b3cbf3e7202a100
banel1ng <ohot2015@gmail.com>
2021-07-31 20:18:37 +0300

    add new bot

--- tgSetWebHook.php
+++ tgSetWebHook.php
@@ -5,12 +5,15 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '1845276413:AAG0BJt1zIxMy18OvvH_c31NOB2asnCQ7XY';
 $bot_username = 'pathePolivBot';
-$hook_url     = 'https://cccxxx.ml/hook.php';
+
+$banel1ng_termostat_bot_bot_api_key ="1918038811:AAHR8inopkXWmXQdKqVbZTQATMpAUdAQTX8";
+$banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
+
+$hook_url     = 'https://cccxxx.ml/tgTermoatatHook.php';
 
 try {
     // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
-
+    $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
     // Set webhook
     $result = $telegram->setWebhook($hook_url);
     if ($result->isOk()) {
---
+++ tgTermoatatHook.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+$banel1ng_termostat_bot_bot_api_key ="1918038811:AAHR8inopkXWmXQdKqVbZTQATMpAUdAQTX8";
+$banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
+
+try {
+    $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
+    $rs = file_get_contents('php://input');
+    $jsonRs = json_decode($rs,true);
+
+    $result = \Longman\TelegramBot\Request::sendMessage([
+        'chat_id' => "-411683583",
+        'text'    =>$jsonRs['message']['text'] ,
+    ]);
+    $telegram->handle();
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e->getMessage();
+}
