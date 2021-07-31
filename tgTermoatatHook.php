8b5f98bd168a24556465994a211e650032b5aa5a
banel1ng <ohot2015@gmail.com>
2021-07-31 20:33:36 +0300

    add new bot

--- tgTermoatatHook.php
+++ tgTermoatatHook.php
@@ -3,11 +3,12 @@ require __DIR__ . '/vendor/autoload.php';
 $banel1ng_termostat_bot_bot_api_key ="1918038811:AAHR8inopkXWmXQdKqVbZTQATMpAUdAQTX8";
 $banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
 
+
 try {
     $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
     $rs = file_get_contents('php://input');
-    $jsonRs = json_decode($rs,true);
-
+    $jsonRs = '';//json_decode($rs,true);
+    file_put_contents('./test.log',$rs);
     $result = \Longman\TelegramBot\Request::sendMessage([
         'chat_id' => "-411683583",
         'text'    =>$jsonRs['message']['text'] ,
