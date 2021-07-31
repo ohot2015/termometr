85b936e5bd27726479c90b72e4f00a25b6298dcd
banel1ng <ohot2015@gmail.com>
2021-07-31 21:25:37 +0300

    add

---
+++ termComand.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+$banel1ng_termostat_bot_bot_api_key ="1918038811:AAHR8inopkXWmXQdKqVbZTQATMpAUdAQTX8";
+$banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
+
+try {
+    $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
+    $data = json_decode(file_get_contents('./termCommand.json'));
+    $log = file_get_contents('php://input');
+
+    $result = \Longman\TelegramBot\Request::sendMessage([
+        'chat_id' => "-411683583",
+        'text'    => 'максимальная температура отправлена на термостат ' . $log,
+    ]);
+
+    echo file_get_contents("termCommand.json");
+
+    $telegram->handle();
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e->getMessage();
+}
