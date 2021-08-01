cb4c0cbcf50deb21033e6bd8a3115e2ad73851eb
banel1ng <ohot2015@gmail.com>
2021-08-01 14:20:56 +0300

    fix

--- index.php
+++ index.php
@@ -17,9 +17,9 @@ $time = [];
 $data = array_slice($data, -50000);
 $superMax = -100;
 $superMin = 150;
-
-for ($i = 0; $i <= count($data);) {
-    $bufT1 =  $bufT2 =  $bufT3 =  $bufT4 = 0;
+//
+//for ($i = 0; $i <= count($data);) {
+//    $bufT1 =  $bufT2 =  $bufT3 =  $bufT4 = 0;
 
 //    for ($j = 0; $j < 10; $j++) {
 //        if (empty($data[$i+$j])) {
@@ -30,7 +30,7 @@ for ($i = 0; $i <= count($data);) {
 //        $bufT3 += $data[$i+$j]['t']['t3'];
 //
 //    }
-    $bufT1 = $data[$i]['t']['t1'];
+/*    $bufT1 = $data[$i]['t']['t1'];
     $bufT2 = $data[$i]['t']['t2'];
     $bufT3 = $data[$i]['t']['t3'];
     $i+=10;
@@ -41,7 +41,7 @@ for ($i = 0; $i <= count($data);) {
 
     $sensor1[] = $bufT1;///10;
     $sensor2[] = $bufT2;///10;
-    $sensor3[] = $bufT3;///10;
+    $sensor3[] = $bufT3;///10;*/
 
 //
 //    if ($superMax < $bufT1/10) {
@@ -64,17 +64,17 @@ for ($i = 0; $i <= count($data);) {
 //        $superMin = $bufT3/10;
 //    }
 
-}
-
-//echo '<br><br><br><br>';
-//foreach ($data as $key => $d) {
-//
-//    $sensor1[] = $d['t']['t1'];
-//    $sensor2[] = $d['t']['t2'];
-//    $sensor3[] = $d['t']['t3'];
-//    $sensor4[] = $d['t']['t4'];
-//    $time[] = $d['time'];
 //}
+
+echo '<br><br><br><br>';
+foreach ($data as $key => $d) {
+
+    $sensor1[] = $d['t']['t1'];
+    $sensor2[] = $d['t']['t2'];
+    $sensor3[] = $d['t']['t3'];
+    $sensor4[] = $d['t']['t4'];
+    $time[] = $d['time'];
+}
 
 $temperature = [$sensor1,$sensor2,$sensor3];
 //var_dump($temperature);
