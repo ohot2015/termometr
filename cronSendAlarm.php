dd3bc851d8265fad353ca12666deffd47542e60e
banel1ng <ohot2015@gmail.com>
2021-10-19 18:36:40 +0300

    alarm cron

---
+++ cronSendAlarm.php
@@ -0,0 +1,49 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+$banel1ng_termostat_bot_bot_api_key ="1918038811:AAHR8inopkXWmXQdKqVbZTQATMpAUdAQTX8";
+$banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
+
+$file = file_get_contents('./log.json');
+$file = json_decode($file,true);
+
+if (!empty($file)) {
+    $lastElemLog = $file[count($file) - 1];
+    $date = new DateTime();
+    $dateFormat = $date->format('H:i:s');
+
+    $dateMyFormat = explode(':',$dateFormat);
+    $d = $dateMyFormat[2] + $dateMyFormat[1]*60 + $dateMyFormat[0]*60*60;
+    $dateMyFormat2 = $lastElemLog['time'];
+
+    $dateMyFormat2= explode(':', $dateMyFormat2);
+
+    $dlog = $dateMyFormat2[2] + $dateMyFormat2[1]*60 + $dateMyFormat2[0]*60*60;
+    file_put_contents('log3.json',  $d - $dlog);
+    if ($d - $dlog > 100) {
+        try {
+            $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
+            $result = \Longman\TelegramBot\Request::sendMessage([
+                'chat_id' => "-411683583",
+                'text'    => $d - $dlog. ' секунд не приходили новые данные' ,
+            ]);
+            file_put_contents('log3.json', $d - $dlog. ' секунд не приходили новые данные' );
+            $telegram->handle();
+        } catch (Longman\TelegramBot\Exception\TelegramException $e) {
+            echo $e->getMessage();
+        }
+    }
+}else {
+    try {
+        $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
+        $result = \Longman\TelegramBot\Request::sendMessage([
+            'chat_id' => "-411683583",
+            'text'    => 'лог пустой' ,
+        ]);
+        file_put_contents('log3.json', ' лог пустой' );
+        $telegram->handle();
+    } catch (Longman\TelegramBot\Exception\TelegramException $e) {
+        echo $e->getMessage();
+    }
+}
+
+?>
--- input.php
+++ input.php
@@ -2,9 +2,7 @@
 ini_set('display_errors', 1);
 $data = file_get_contents('php://input');
 $file = file_get_contents('./log.json');
-require __DIR__ . '/vendor/autoload.php';
-$banel1ng_termostat_bot_bot_api_key ="1918038811:AAHR8inopkXWmXQdKqVbZTQATMpAUdAQTX8";
-$banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
+
 
 if (!empty($file)) {
     $file = json_decode($file,true);
@@ -14,31 +12,7 @@ if (!empty($file)) {
     $data = json_decode($data,true);
 
     $file = array_merge($file, $data);
-    $lastElemLog = $file[count($file) - 1];
-    $date = new DateTime();
-    $dateFormat = $date->format('H:i:s');
 
-    $dateMyFormat = explode(':',$dateFormat);
-    $d = $dateMyFormat[2] + $dateMyFormat[1]*60 + $dateMyFormat[0]*60*60;
-    $dateMyFormat2 = $lastElemLog['time'];
-
-    $dateMyFormat2= explode(':', $dateMyFormat2);
-
-    $dlog = $dateMyFormat2[2] + $dateMyFormat2[1]*60 + $dateMyFormat2[0]*60*60;
-    file_put_contents('log3.json',  $d - $dlog);
-    if ($d - $dlog > 100) {
-        try {
-            $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
-            $result = \Longman\TelegramBot\Request::sendMessage([
-                'chat_id' => "-411683583",
-                'text'    => $d - $dlog. 'не приходили новые данные' ,
-            ]);
-            file_put_contents('log3.json', $d - $dlog. ' секунд не приходили новые данные' );
-            $telegram->handle();
-        } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-            echo $e->getMessage();
-        }
-    }
     $file = json_encode($file);
 
 
