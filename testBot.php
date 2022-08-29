654fecf214092f1e303675e09b8222cab1ff922b
ohot2015@gmail.com <B41b5ieq!>
2022-08-29 11:49:50 +0300

    add test bot file

--- testBot.php
+++ testBot.php
@@ -1,17 +1,18 @@
 <?php
+error_reporting(E_ALL);
 require __DIR__ . '/vendor/autoload.php';
 $banel1ng_termostat_bot_bot_api_key ="1918038811:AAHR8inopkXWmXQdKqVbZTQATMpAUdAQTX8";
 $banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
 
 try {
-    $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
+    //$telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
 
     $result = \Longman\TelegramBot\Request::sendMessage([
         'chat_id' => "-411683583",
         'text'    => 'test',
     ]);
 
-    $telegram->handle();
+    //$telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     echo $e->getMessage();
 }
