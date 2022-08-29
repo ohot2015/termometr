c143ee7e93e0c7f88db12924a4e006c8589a393a
ohot2015@gmail.com <B41b5ieq!>
2022-08-29 10:47:41 +0300

    fix url bot tg

--- tgSetWebHook.php
+++ tgSetWebHook.php
@@ -4,16 +4,16 @@ error_reporting(E_ALL);
 require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '1845276413:AAG0BJt1zIxMy18OvvH_c31NOB2asnCQ7XY';
-$bot_username = 'pathePolivBot';
+$bot_username = 'banel1ng_termostat_bot';
 
 $banel1ng_termostat_bot_bot_api_key ="1918038811:AAHR8inopkXWmXQdKqVbZTQATMpAUdAQTX8";
 $banel1ng_termostat_bot_username = 'banel1ng_termostat_bot';
 
-$hook_url     = 'https://cccxxx.ml/tgTermoatatHook.php';
+$hook_url     = 'https://pribor-kotel.ru/tgTermoatatHook.php';
 
 try {
     // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($banel1ng_termostat_bot_bot_api_key, $banel1ng_termostat_bot_username);
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $banel1ng_termostat_bot_username);
     // Set webhook
     $result = $telegram->setWebhook($hook_url);
     if ($result->isOk()) {
